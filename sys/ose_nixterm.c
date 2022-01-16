3b36adbdc5ad487979bd60258f33f412eabac6a2
maccallum <john.m@ccallum.com>
2022-01-16 13:15:57 +0100

    Improved line editing with lined.
    Added support for escape (meta) sequences. This involved
    changing the ose_termRead function to take a buffer and max
    number of chars to read. Ultimately, the reading is happening
    in the wrong place--it should be done by the host, and
    ose_lined_read should read things from the stack, not a file
    descriptor. To be refactored in a future commit.
    
    Added M^b (jump back), M^f (jump forward), and M^DEL (delete word).
    In this commit, the only word-break character is '/', and it's
    hardcoded. In a future commit, we'll use a list of chars that's
    setable.
    
    Also added C^d for deleting the character under the cursor.

--- sys/ose_nixterm.c
+++ sys/ose_nixterm.c
@@ -1,12 +1,12 @@
 /*
-  Copyright (c) 2019-21 John MacCallum Permission is hereby granted,
-  free of charge, to any person obtaining a copy of this software and
-  associated documentation files (the "Software"), to deal in the
-  Software without restriction, including without limitation the
-  rights to use, copy, modify, merge, publish, distribute, sublicense,
-  and/or sell copies of the Software, and to permit persons to whom
-  the Software is furnished to do so, subject to the following
-  conditions:
+  Copyright (c) 2019-22 John MacCallum Permission is hereby granted,
+  free of charge, to any person obtaining a copy of this software
+  and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute,
+  sublicense, and/or sell copies of the Software, and to permit
+  persons to whom the Software is furnished to do so, subject to the
+  following conditions:
 
   The above copyright notice and this permission notice shall be
   included in all copies or substantial portions of the Software.
@@ -14,11 +14,11 @@
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
 */
 
 #include <stdio.h>
@@ -95,14 +95,19 @@ fatal:
     return -1;
 }
 
-int ose_termRead(void)
+/* int ose_termRead(void) */
+/* { */
+/*     unsigned char c; */
+/*     fflush(stdout); */
+/*     /\* ose_termRaw(); *\/ */
+/*     if(read(STDIN_FILENO, &c, 1) == 1) */
+/*     { */
+/*         return (unsigned int)c; */
+/*     } */
+/*     return 0; */
+/* } */
+
+int32_t ose_termRead(int32_t buflen, char *buf)
 {
-    unsigned char c;
-    fflush(stdout);
-    /* ose_termRaw(); */
-    if(read(STDIN_FILENO, &c, 1) == 1)
-    {
-        return (unsigned int)c;
-    }
-    return 0;
+    return read(STDIN_FILENO, buf, buflen);
 }
